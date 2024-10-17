14b70d80818ee87e411ca548d3bdf33a46dc9896
saratomaz <sara.tomaz@iohk.io>
2024-10-17 11:30:10 +0100

    Remove unnecessary files for sync tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="cardano-node-tests",
+    name="cardano-sync-tests",
     packages=find_packages(),
     use_scm_version=True,
 )
--- src_docs/source/modules.rst
+++
@@ -1,7 +0,0 @@
-Source Documentation
-====================
-
-.. toctree::
-   :maxdepth: 4
-
-   cardano_node_tests
