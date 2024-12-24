487ec5e96fad13289b1bb77714a95f667ba7d3fd
saratomaz <sara.tomaz@iohk.io>
2024-12-24 15:07:59 +0000

    Refactor refactor_gitpython_utils

--- sync_tests/utils/gitpython_utils.py
+++ sync_tests/utils/gitpython_utils.py
@@ -1,23 +1,45 @@
 import os
-from git import Repo
+import logging
 
+from git import Repo
+from git.exc import GitCommandError
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def git_clone_iohk_repo(repo_name, repo_dir, repo_branch):
-    repo = Repo.clone_from(f"https://github.com/input-output-hk/{repo_name}.git", repo_dir)
-    repo.git.checkout(repo_branch)
-    print(f"Repo: {repo_name} cloned to: {repo_dir}")
-    return repo
+    """Clones an IOHK repository and checks out a specific branch."""
+
+    try:
+        repo = Repo.clone_from(f"https://github.com/input-output-hk/{repo_name}.git", repo_dir)
+        repo.git.checkout(repo_branch)
+        logging.info(f"Repository {repo_name} successfully cloned to {repo_dir} and branch {repo_branch} checked out.")
+        return repo
+    except GitCommandError as e:
+        logging.error(f"Error cloning repository {repo_name}: {e}")
+        raise
 
 
 def git_checkout(repo, rev):
-    print(f"Checked out rev: {rev} of {repo}")
-    repo.git.checkout(rev)
-    return repo
+    """Checks out a specific revision in the given repository."""
+    try:
+        repo.git.checkout(rev)
+        logging.info(f"Checked out revision {rev} in repository: {repo.working_dir}")
+        return repo
+    except GitCommandError as e:
+        logging.error(f"Error checking out revision {rev} in repository {repo.working_dir}: {e}")
+        raise
 
 
 def clone_repo(repo_name, repo_branch):
-    location = os.getcwd() + f"/{repo_name}"
-    repo = Repo.clone_from(f"https://github.com/input-output-hk/{repo_name}.git", location)
-    repo.git.checkout(repo_branch)
-    print(f"Repo: {repo_name} cloned to: {location}")
-    return location
+    """Clones a repository and checks out a specific branch"""
+    location = os.path.join(os.getcwd(), repo_name)
+    try:
+        repo = Repo.clone_from(f"https://github.com/input-output-hk/{repo_name}.git", location)
+        repo.git.checkout(repo_branch)
+        logging.info(
+            f"Repository {repo_name} successfully cloned to {location} and branch {repo_branch} checked out.")
+        return location
+    except GitCommandError as e:
+        logging.error(f"Error cloning repository {repo_name} to {location}: {e}")
+        raise
