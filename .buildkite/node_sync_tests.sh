21263cf30c09ba46911f491fdb8c96a2cde743cb
saratomaz <sara.tomaz@iohk.io>
2025-01-17 21:54:50 +0000

    Enable Genesis mode - transitory approach

--- .buildkite/node_sync_tests.sh
+++ .buildkite/node_sync_tests.sh
@@ -27,18 +27,18 @@ echo " ==== importing packages from nix (https://search.nixos.org/packages)"
 python3 -c "import requests, pandas, psutil, pymysql;"
 
 env=$1
-build_mode=$2
-node_rev1=$3
-tag_no1=$4
-node_topology1=$5
-node_start_arguments1=$6
-node_rev2=$7
-tag_no2=$8
-node_topology2=$9
-node_start_arguments2=${10}
+node_rev1=$2
+tag_no1=$3
+node_topology1=$4
+node_start_arguments1=$5
+node_rev2=$6
+tag_no2=$7
+node_topology2=$8
+node_start_arguments2=$9
+use_genesis_mode=${10}
 
 echo " ==== start sync test"
-python ./sync_tests/tests/node_sync_test.py -b "$build_mode" -e "$env" -t1 "$tag_no1" -t2 "$tag_no2" -r1 "$node_rev1" -r2 "$node_rev2" -n1 "$node_topology1" -n2 "$node_topology2" -a1="$node_start_arguments1" -a2="$node_start_arguments2"
+python ./sync_tests/tests/node_sync_test.py -e "$env" -t1 "$tag_no1" -t2 "$tag_no2" -r1 "$node_rev1" -r2 "$node_rev2" -n1 "$node_topology1" -n2 "$node_topology2" -a1="$node_start_arguments1" -a2="$node_start_arguments2" -g "${use_genesis_mode}"
 
 echo "--- Prepare for adding sync test results to the AWS Database"
 python ./sync_tests/tests/node_write_sync_values_to_db.py -e "$env"
