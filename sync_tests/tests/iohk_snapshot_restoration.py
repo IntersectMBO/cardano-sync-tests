9fc26148b703a1d1be3e24cdc4c5815b9e1f8bc6
saratomaz <sara.tomaz@iohk.io>
2025-02-19 20:48:22 +0000

    Refactor print statements to use logger

--- sync_tests/tests/iohk_snapshot_restoration.py
+++ sync_tests/tests/iohk_snapshot_restoration.py
@@ -11,19 +11,19 @@ from collections import OrderedDict
 sys.path.append(os.getcwd())
 
 from sync_tests.utils import aws_db
+from sync_tests.utils import color_logger
 from sync_tests.utils import db_sync
 from sync_tests.utils import gitpython
 from sync_tests.utils import helpers
 from sync_tests.utils import node
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+LOGGER = logging.getLogger(__name__)
 
 TEST_RESULTS = "db_sync_iohk_snapshot_restoration_test_results.json"
 
 
 def upload_snapshot_restoration_results_to_aws(env: str) -> None:
-    print("--- Write IOHK snapshot restoration results to AWS Database")
+    LOGGER.info("--- Write IOHK snapshot restoration results to AWS Database")
     with open(TEST_RESULTS) as json_file:
         sync_test_results_dict = json.load(json_file)
 
@@ -34,48 +34,47 @@ def upload_snapshot_restoration_results_to_aws(env: str) -> None:
     identifier = env + "_restoration_" + test_id
     sync_test_results_dict["identifier"] = identifier
 
-    print(f"  ==== Write test values into the {test_summary_table} DB table:")
+    LOGGER.info(f"  ==== Write test values into the {test_summary_table} DB table:")
     col_to_insert = list(sync_test_results_dict.keys())
     val_to_insert = list(sync_test_results_dict.values())
 
     if not aws_db.insert_values_into_db(test_summary_table, col_to_insert, val_to_insert):
-        print(f"col_to_insert: {col_to_insert}")
-        print(f"val_to_insert: {val_to_insert}")
+        LOGGER.error(f"Failed to insert values into {test_summary_table}")
         sys.exit(1)
 
 
 def run_test(args: argparse.Namespace) -> None:
-    print("--- Db-sync restoration from IOHK official snapshot")
+    LOGGER.info("--- Db-sync restoration from IOHK official snapshot")
     platform_system, platform_release, platform_version = helpers.get_os_type()
-    print(f"Platform: {platform_system, platform_release, platform_version}")
+    LOGGER.info(f"Platform: {platform_system, platform_release, platform_version}")
 
     start_test_time = datetime.datetime.now(tz=datetime.timezone.utc).strftime("%d/%m/%Y %H:%M:%S")
-    print(f"Test start time: {start_test_time}")
+    LOGGER.info(f"Test start time: {start_test_time}")
 
     env = helpers.get_arg_value(args=args, key="environment")
-    print(f"Environment: {env}")
+    LOGGER.info(f"Environment: {env}")
 
     node_pr = helpers.get_arg_value(args=args, key="node_pr", default="")
-    print(f"Node PR number: {node_pr}")
+    LOGGER.info(f"Node PR number: {node_pr}")
 
     node_branch = helpers.get_arg_value(args=args, key="node_branch", default="")
-    print(f"Node branch: {node_branch}")
+    LOGGER.info(f"Node branch: {node_branch}")
 
     node_version_from_gh_action = helpers.get_arg_value(
         args=args, key="node_version_gh_action", default=""
     )
-    print(f"Node version: {node_version_from_gh_action}")
+    LOGGER.info(f"Node version: {node_version_from_gh_action}")
 
     db_branch = helpers.get_arg_value(args=args, key="db_sync_branch", default="")
-    print(f"DB sync branch: {db_branch}")
+    LOGGER.info(f"DB sync branch: {db_branch}")
 
     db_sync_version_from_gh_action = helpers.get_arg_value(
         args=args, key="db_sync_version_gh_action", default=""
     )
-    print(f"DB sync version: {db_sync_version_from_gh_action}")
+    LOGGER.info(f"DB sync version: {db_sync_version_from_gh_action}")
 
     snapshot_url = db_sync.get_latest_snapshot_url(env, args)
-    print(f"Snapshot url: {snapshot_url}")
+    LOGGER.info(f"Snapshot url: {snapshot_url}")
 
     # cardano-node setup
     conf_dir = pl.Path.cwd()
@@ -98,7 +97,7 @@ def run_test(args: argparse.Namespace) -> None:
     node.configure_node(config_file=conf_dir / "config.json")
     node.start_node(base_dir=base_dir, node_start_arguments=())
     node.wait_node_start(env=env, timeout_minutes=10)
-    print("--- Node startup", flush=True)
+    LOGGER.info("--- Node startup")
     db_sync.print_file(db_sync.NODE_LOG_FILE, 80)
     (
         sync_time_seconds,
@@ -109,7 +108,7 @@ def run_test(args: argparse.Namespace) -> None:
     ) = node.wait_for_node_to_sync(env=env, base_dir=base_dir)
 
     # cardano-db sync setup
-    print("--- Db sync setup")
+    LOGGER.info("--- Db sync setup")
     os.chdir(db_sync.ROOT_TEST_PATH)
     db_sync_dir = gitpython.clone_repo("cardano-db-sync", db_branch)
     os.chdir(db_sync_dir)
@@ -119,7 +118,7 @@ def run_test(args: argparse.Namespace) -> None:
     db_sync.list_databases()
     helpers.execute_command("nix build .#cardano-db-sync -o db-sync-node")
     helpers.execute_command("nix build .#cardano-db-tool -o db-sync-tool")
-    print("--- Download and check db-sync snapshot", flush=True)
+    LOGGER.info("--- Download and check db-sync snapshot")
     db_sync.copy_db_sync_executables(build_method="nix")
     snapshot_name = db_sync.download_db_sync_snapshot(snapshot_url)
     expected_snapshot_sha_256_sum = db_sync.get_snapshot_sha_256_sum(snapshot_url)
@@ -127,28 +126,28 @@ def run_test(args: argparse.Namespace) -> None:
     assert expected_snapshot_sha_256_sum == actual_snapshot_sha_256_sum, "Incorrect sha 256 sum"
 
     # restore snapshot
-    print("--- Snapshot restoration")
+    LOGGER.info("--- Snapshot restoration")
     restoration_time = db_sync.restore_db_sync_from_snapshot(
         env, snapshot_name, remove_ledger_dir="no"
     )
-    print(f"Restoration time [sec]: {restoration_time}")
+    LOGGER.info(f"Restoration time [sec]: {restoration_time}")
     db_sync_tip = db_sync.get_db_sync_tip(env)
     assert db_sync_tip is not None  # TODO: refactor
     snapshot_epoch_no, snapshot_block_no, snapshot_slot_no = db_sync_tip
-    print(
+    LOGGER.info(
         f"db-sync tip after restoration: epoch: {snapshot_epoch_no}, "
         f"block: {snapshot_block_no}, slot: {snapshot_slot_no}"
     )
 
     # start db-sync
-    print("--- Db sync start")
+    LOGGER.info("--- Db sync start")
     db_sync.start_db_sync(env, start_args="", first_start="True")
     db_sync.print_file(db_sync.DB_SYNC_LOG_FILE, 30)
     db_sync_version, db_sync_git_rev = db_sync.get_db_sync_version()
     db_full_sync_time_in_secs = db_sync.wait_for_db_to_sync(env)
     end_test_time = datetime.datetime.now(tz=datetime.timezone.utc).strftime("%d/%m/%Y %H:%M:%S")
     wait_time = 30
-    print(f"Waiting for additional {wait_time} minutes to continue syncying...")
+    LOGGER.info(f"Waiting for additional {wait_time} minutes to continue syncying...")
     db_sync.wait(wait_time * db_sync.ONE_MINUTE)
     db_sync.print_file(db_sync.DB_SYNC_LOG_FILE, 60)
     db_sync_tip = db_sync.get_db_sync_tip(env)
@@ -156,12 +155,12 @@ def run_test(args: argparse.Namespace) -> None:
     epoch_no, block_no, slot_no = db_sync_tip
 
     # shut down services
-    print("--- Stop cardano services")
+    LOGGER.info("--- Stop cardano services")
     helpers.manage_process(proc_name="cardano-db-sync", action="terminate")
     helpers.manage_process(proc_name="cardano-node", action="terminate")
 
     # export test data as a json file
-    print("--- Gathering end results")
+    LOGGER.info("--- Gathering end results")
     test_data: OrderedDict[str, tp.Any] = OrderedDict()
     test_data["platform_system"] = platform_system
     test_data["platform_release"] = platform_release
@@ -226,7 +225,7 @@ def run_test(args: argparse.Namespace) -> None:
     upload_snapshot_restoration_results_to_aws(env)
 
     # search db-sync log for issues
-    print("--- Summary: Rollbacks, errors and other isssues")
+    LOGGER.info("--- Summary: Rollbacks, errors and other isssues")
 
     log_errors = db_sync.are_errors_present_in_db_sync_logs(db_sync.DB_SYNC_LOG_FILE)
     db_sync.print_color_log(db_sync.sh_colors.WARNING, f"Are errors present: {log_errors}")
@@ -289,6 +288,7 @@ def get_args() -> argparse.Namespace:
 
 
 def main() -> int:
+    logging.setLoggerClass(color_logger.ColorLogger)
     args = get_args()
     run_test(args=args)
 
